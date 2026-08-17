60c6116cb01d29ee28845cd8f481a370779d6fd7
alvansga <zralvansga97@gmail.com>
2026-08-17 21:46:25 +0700

    error terkait cache

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.StaticFiles;
 using WebAppSandbox.Hubs;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,7 +9,18 @@ builder.Services.AddSingleton<RoomManager>();
 var app = builder.Build();
 
 app.UseDefaultFiles();
-app.UseStaticFiles();
+app.UseStaticFiles(new StaticFileOptions
+{
+    OnPrepareResponse = ctx =>
+    {
+        var name = ctx.File?.Name ?? "";
+        if (name.EndsWith(".js", StringComparison.OrdinalIgnoreCase) ||
+            name.EndsWith(".css", StringComparison.OrdinalIgnoreCase))
+        {
+            ctx.Context.Response.Headers.CacheControl = "no-cache, no-store";
+        }
+    }
+});
 
 app.MapHub<GameHub>("/hubs/game");
 
